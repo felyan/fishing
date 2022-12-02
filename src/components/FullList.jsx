2c7cc245161c77313764fb71b984b2278e6efb46
Eva Szilardne Kallai <kallaiszilardne@gmail.com>
2022-12-02 11:57:07 +0100

    FullList, TotalCatching

--- src/components/FullList.jsx
+++ src/components/FullList.jsx
@@ -1,36 +1,40 @@
 import React from 'react'
 import styled from 'styled-components';
+import Section from './Section';
 
 
 const FullList = () => {  
   const fisherman = ["József", "János", "Géza", "Béla", "András", "Tamás"];
-const catching = [8, 4, 6, 7, 5, 0];
+  const catching = [8, 4, 6, 7, 5, 0];
 
   let totalFisherman = fisherman.length;
-  let result = "";
+  let result = 0;
+
   
-  for (let i = 0; i < fisherman.length; i++) {
-    for (let j = 0; j < catching.length; j++) {
+  
+  for (let j = 0; j < catching.length; j++) {
+    result += catching[j];    
   }
   
-      result += `${fisherman[i]} ${catching[i]} halat fogott. `;
-}
+  const text = `A horgászversenyen ${totalFisherman} horgász indult.
+                ${fisherman}
+                Összesen ${ result } halat fogtak.`;
 
   return (
+    <Container>
+      <Section
+        title="Résztvevők"
+        description={text}
+        backgroundImg="https://images.unsplash.com/photo-1657194724857-117a10f56baf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+      />
+    </Container>
     
-    <Section>
-      <p>{totalFisherman} horgász indult a horgászversenyen.</p>
-      <p>{result}</p>
-
-
-
-    </Section>
   )
 }
 
 export default FullList
 
 
-const Section = styled.div`
-
+const Container = styled.div`
+  height: 100vh;
 `
--- src/components/TotalCatching.jsx
+++ src/components/TotalCatching.jsx
@@ -1,49 +1,28 @@
 import React from 'react'
-import {
-    Box,
-    Card,
-    CardContent,
-    Typography,
-    CardActions,
-    Button,
-    CardMedia,
-} from '@mui/material';
 import styled from 'styled-components';
+import Section from './Section';
 
 const TotalCatching = () => {
-  const fisherman = ["József", "János", "Géza", "Béla", "András"];
-  const catching = [8, 4, 6, 7, 5];
+  const fisherman = ["József", "János", "Géza", "Béla", "András", "Tamás"];
+  const catching = [8, 4, 6, 7, 5, 0];
 
   let totalCatching = "";
 
   for (let i = 0; i < fisherman.length; i++) {
   for (let j = 0; j < catching.length; j++) {}
-  totalCatching=`${fisherman[i]} ${catching[i]} halat fogott`;
+  totalCatching+=`${fisherman[i]} ${catching[i]} halat fogott`;
   }
+
+  //result += `${fisherman[i]} ${catching[i]} halat fogott. `;
   
   return (
-    <Box width='95%' height='900px' m={5} >
-          <Card>
-              <CardMedia
-                  component='img'
-                  height='440'
-                  image='https://images.unsplash.com/photo-1534948216015-843149f72be3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
-                  alt='unsplash image'
-              />
-              <CardContent>
-                  <Typography gutterBottom variant='h5' component='div'>
-                      Össz fogás
-                  </Typography>
-                  <Typography variant='body2' color='text-secondary'>
-                    {totalCatching}
-                  </Typography>
-              </CardContent>
-              <CardActions>
-                  <Button size='small'>Share</Button>
-                  <Button size='small'>Learn more</Button>
-              </CardActions>
-          </Card>
-    </Box>
+    <Container>
+      <Section
+        title="XXVII. Horgászverseny"
+        description={totalCatching}
+        backgroundImg="https://images.unsplash.com/photo-1657194724857-117a10f56baf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+      />
+    </Container>
 
     // <Section>
     //   <h1>3. Feladat</h1>
@@ -54,6 +33,6 @@ const TotalCatching = () => {
 
 export default TotalCatching
 
-const Section = styled.div`
-  margin-bottom: 50px;
+const Container = styled.div`
+  height: 100vh;
 `
